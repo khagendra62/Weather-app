340541a83def5dfecfe31daa0646e7ed9c88a2ca
khagendra62 <khagendraawasthi47@gmail.com>
2025-01-25 21:25:05 +0545

    implement api

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-const randomWeather = {
-  coord: { lon: 85.3167, lat: 27.7167 },
-  weather: [
-    { id: 801, main: "Clouds", description: "few clouds", icon: "02n" },
-  ],
-  base: "stations",
-  main: {
-    temp: 9.12,
-    feels_like: 9.12,
-    temp_min: 9.12,
-    temp_max: 9.12,
-    pressure: 1014,
-    humidity: 71,
-    sea_level: 1014,
-    grnd_level: 849,
-  },
-  visibility: 7000,
-  wind: { speed: 1.03, deg: 0 },
-  clouds: { all: 20 },
-  dt: 1737747760,
-  sys: {
-    type: 1,
-    id: 9201,
-    country: "NP",
-    sunrise: 1737767309,
-    sunset: 1737805996,
-  },
-  timezone: 20700,
-  id: 1283240,
-  name: "Kathmandu",
-  cod: 200,
-};
-
 const weatherMain = document.getElementById("weatherMain");
 const temperatureValue = document.getElementById("temperatureValue");
 const temperatureMinMax = document.getElementById("temperatureMinMax");
@@ -39,11 +6,9 @@ const humidityPercentage = document.getElementById("humidityPercentage");
 const windSpeed = document.getElementById("windSpeed");
 const pressureValue = document.getElementById("pressureValue");
 const searchBtn = document.getElementById("searchBtn");
+const searchCity = document.getElementById("searchCity");
 
-searchBtn.addEventListener("click", () => {
-  updatePage(randomWeather);
-  console.log("works");
-});
+searchBtn.addEventListener("click", () => onSearch());
 
 function updatePage(weatherData) {
   weatherMain.innerText = weatherData.weather[0].main;
@@ -53,4 +18,18 @@ function updatePage(weatherData) {
   humidityPercentage.innerText = `${weatherData.main.humidity} %`;
   windSpeed.innerText = `${weatherData.wind.speed} m/s`;
   pressureValue.innerText = `${weatherData.main.pressure} mm of Hg`;
+}
+
+function onSearch() {
+  const city = searchCity.value;
+  if (!city) return;
+  getWeather(city);
+}
+
+async function getWeather(location) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=9e29402da1d0377e04d1e1503b4d8046&units=metric`
+  )
+    .then((response) => response.json())
+    .then((parsedData) => updatePage(parsedData));
 }
